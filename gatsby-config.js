8a4cd85f7c9a2d13bd63ce98ed88ab6d6d807f15
hebasaadosman <68795300+hebasaadosman@users.noreply.github.com>
2020-11-04 21:58:28 +0300

    save arezzo files

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,10 +1,56 @@
-/**
- * Configure your Gatsby site with this file.
- *
- * See: https://www.gatsbyjs.com/docs/gatsby-config/
- */
-
 module.exports = {
-  /* Your site config here */
-  plugins: [],
+  
+    siteMetadata: {
+      title: `Heba Saad`,
+      description:"Walink is the top WhatsApp link shortener tool in the world. Create links with custom message, wa.link domain and its own QR code for users to chat with you instantly without having your phone number in their address book."
+    },
+  
+  plugins: [
+    
+    
+    
+    {
+
+      resolve: `gatsby-plugin-styletron`,
+      options: {
+        // You can pass options to Styletron.
+        prefix: "Heba-",
+        // Disable dev debug mode, enabled by default
+        debug: false,
+      },
+    },
+    
+    {
+      resolve: `gatsby-plugin-canonical-urls`,
+      options: {
+        siteUrl: `http://localhost:8000`,
+      },
+    },
+    {
+      resolve: `gatsby-plugin-google-analytics`,
+      options: {
+        // The property ID; the tracking code won't be generated without it
+        trackingId: "UA-154726194-2",
+        // Defines where to place the tracking script - `true` in the head and `false` in the body
+        head: true,
+      }
+    },
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: "Walink",
+        short_name: "Walink",
+        start_url: "/",
+        background_color: "#fff",
+        theme_color: "#fff",
+        // Enables "Add to Homescreen" prompt and disables browser UI (including back button)
+        // see https://developers.google.com/web/fundamentals/web-app-manifest/#display
+        display: "minimal-ui",
+        icon: "src/images/icon-web.png", // This path is relative to the root of the site.
+        // An optional attribute which provides support for CORS check.
+        // If you do not provide a crossOrigin option, it will skip CORS for manifest.
+        // Any invalid keyword or empty string defaults to `anonymous`
+      },
+    },
+  ],
 }
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,117 @@
-import React from "react"
+import React, { useState } from 'react'
+import { Link } from 'gatsby'
+import { H2, Paragraph2 } from 'baseui/typography'
+import { useStyletron } from 'baseui'
+import Layout from '../components/layout'
+import GeneratorWrapper from '../components/GeneratorWrapper'
+import Faq from '../components/faq'
+import SEO from '../components/seo'
+import Steps from '../components/steps'
+import Hero from '../components/hero'
+import CTA from '../components/RegisterCTA'
 
-export default function Home() {
-  return <div>Hello world!</div>
+const IndexPage = () => {
+  const [css] = useStyletron()
+  const [focusElement, setFocusElement] = useState('')
+
+  const centerText = css({
+    display: 'flex',
+    justifyContent: 'center',
+  })
+
+  return (
+    <Layout hero={<Hero />}>
+      <SEO
+        title="Create WhatsApp links"
+        hreflang={[
+          { rel: 'alternate', hrefLang: 'en', href: 'http://localhost:8000' },
+          { rel: 'alternate', hrefLang: 'es', href: 'https://crear.wa.link' },
+          { rel: 'alternate', hrefLang: 'pt', href: 'https://criar.wa.link' },
+          {
+            rel: 'alternate',
+            hrefLang: 'x-default',
+            href: 'http://http://localhost:8000/',
+          },
+        ]}
+      />
+      <div
+        className={css({
+          margin: '4rem 0',
+        })}
+      >
+        <h2
+          className={css({
+            textAlign: 'center',
+          })}
+        >
+          What is a WhatsApp link?
+        </h2>
+        <div className={centerText}>
+          <Paragraph2>
+            WhatsApp has a feature that allows people to create links, so any
+            user can click and start a conversation (chat) directly with the
+            person who created the link. This functionality is called{' '}
+            <strong>"click to chat"</strong> and as you don’t have to save
+            anyone’s phone number to message them, it simplifies interaction.
+          </Paragraph2>
+        </div>
+        <div className={centerText}>
+          <Paragraph2>
+            Even so, creating WhatsApp links is not user friendly and
+            occasionally takes more time than expected. There’s where Walink
+            comes in, we provide a tool which you can use to generate shortened
+            WhatsApp links, with the <strong>wa.link</strong> domain. This will
+            increase your chat conversions, and it’s free!
+          </Paragraph2>
+        </div>
+      </div>
+      <div
+        className={css({
+          display: 'flex',
+          justifyContent: 'center',
+          margin: '50px auto',
+          background: 'linear-gradient(-45deg, #23a6d5, #23d5ab)',
+          minHeight: '310px',
+          boxShadow: 'rgba(0, 0, 0, 0.16) 0px 1px 4px',
+          borderRadius: '10px',
+        })}
+      >
+        <Steps changeFocus={setFocusElement} />
+      </div>
+      <div
+        id="create-link"
+        className={css({
+          paddingTop: '10px',
+        })}
+      >
+        <H2 className={css({ textAlign: 'center' })}>Create your link here</H2>
+        <GeneratorWrapper focusElement={focusElement} />
+      </div>
+      <div className={css({ marginTop: '50px' })}>
+        <div className={css({ margin: '20px' })}>
+          <H2>FAQs - Frequently Asked Questions:</H2>
+        </div>
+        <Faq />
+        <CTA
+          title="Do you want a branded link?"
+          description={
+            <>
+              With{' '}
+              <Link
+                to="/premium"
+                style={{ color: '#343434', textDecoration: 'none' }}
+              >
+                Walink premium
+              </Link>{' '}
+              you can create, track and modify your custom WhatsApp links.{' '}
+              <strong>(wa.link/YourBrand)</strong>
+            </>
+          }
+          currentPage="Home"
+        />
+      </div>
+    </Layout>
+  )
 }
+
+export default IndexPage
